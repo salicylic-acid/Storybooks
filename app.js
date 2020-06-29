fa7c2427ec323c0a09fdb86323d052594bda19b5
Derick Alfaro <theroadhouse@protonmail.com>
2020-06-29 10:00:48 -0400

    routes

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express')
-const dotenv = require('dotenv').config({path: './config/config.env'})
+const dotenv = require('dotenv').config({
+  path: './config/config.env'
+})
 const connectDB = require('./config/db')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
-
-connectDB()
-
 const app = express()
 const PORT = process.env.PORT || 5000
-
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+connectDB()
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}))
 app.set('view engine', '.hbs')
+
+app.use('/', require('./routes/index'))
+
 
 app.listen(PORT, console.log(`Running in ${process.env.NODE_ENV} mode on port ${PORT}`))
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,27 @@ const express = require('express')
 const router = express.Router()
 
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
+  console.log('Time: ', Date.now())
   res.send('Login')
 })
 
-router.get('/dashboard', (req,res) => {
+router.get('/dashboard', (req, res) => {
+  console.log(`Time: ${Date.now()}`)
   res.send('Dashboard')
+})
+
+router.get('/about', (req, res) => {
+  console.log(req)
+  console.log('...........')
+  console.log(res)
+  res.send('About')
+})
+
+router.post('/submit', (req, res) => {
+  const post = req.body
+  console.log('postSubmit', req)
+  res.send('Testing')
 })
 
 module.exports = router
